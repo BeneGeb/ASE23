a5061cfada8188171841afabb3d3493dc033f034
BeneGeb <92448111+BeneGeb@users.noreply.github.com>
2023-11-01 09:48:10 +0100

    Creating Project structure
    Erstellung einer initialen Projektstruktur mit Websocket Verbindung von React Frontend zu Django Backend

---
+++ blokus_react/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Chat from './components/chat/Chat'
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Chat />
+      </div>
+    )
+  }
+
+}
+
+export default App
