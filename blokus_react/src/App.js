bcf50fb464a761fb2e8348a08d833b98abd8e6a2
Yeaulo <yichenzhang929@gmail.com>
2023-11-02 17:43:10 +0100

    Login Frontend

--- blokus_react/src/App.js
+++ blokus_react/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import Login from './components/login/login'
 import Chat from './components/chat/Chat'
 
 export class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Chat />
+        <Login />
       </div>
     )
   }
