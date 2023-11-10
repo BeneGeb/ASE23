85e4029f34ccd6d669af61893b851682c2fcb7ea
Samuel2k1 <91870323+Samuel2k1@users.noreply.github.com>
2023-11-10 18:53:09 +0100

    Login/Register Frontend

--- blokus_react/src/App.js
+++ blokus_react/src/App.js
@@ -3,13 +3,14 @@ import React from 'react'
 import Chat from './components/chat/Chat'
 import GamePage from './components/gamepage/gamepage'
 import Login from './components/login/login'
+import Dnd from './components/field/dnd'
 
 
 export class App extends React.Component {
   render() {
     return (
       <div className="App" style={{ width: '100%', height: '100vh' }}>
-        <GamePage  />
+        <Dnd  />
       </div>
     )
   }
