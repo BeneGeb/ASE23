0fb1ba188bef39d4c9831a5f64a101610bba1871
BeneGeb <92448111+BeneGeb@users.noreply.github.com>
2023-11-25 12:16:45 +0100

    Einfügen einer Testmöglichkeit für die Websocket verbindung

--- blokus_react/src/components/gamepage/gamepage.js
+++ blokus_react/src/components/gamepage/gamepage.js
@@ -26,20 +26,45 @@ export default function GamePage(){
         setsquaresArray(field)
     }
 
+    //NUR ZUM TESTEN----------
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (event) => {
+        // Den Wert des Eingabefelds aktualisieren, wenn sich der Benutzer ändert
+        setInputValue(event.target.value);
+    };    
+
+    const handleButtonClick = () => {
+    try {
+      // Versuche den eingegebenen String in ein Array umzuwandeln
+      const arrayValue = JSON.parse(inputValue);
+
+      sendPlacedBlock(arrayValue, allPlayer.find(player => player.player_id ==currPlayer).color);
+      // Hier kannst du das Array verwenden
+      console.log('Aktueller Wert des Inputs als Array:', arrayValue);
+
+      // Hier kannst du deine WebSocket-Funktion mit dem Array-Wert aufrufen
+      // sendPlacedBlock(arrayValue, "red");
+    } catch (error) {
+      console.error('Fehler beim Parsen des Eingabewerts als Array:', error);
+    }
+  };
+    //------------------------
 
     return (
         <div class="gamepage">
-            <button onClick={() => sendPlacedBlock([4,8,9],"red")} />
             <div className="left-overviews">
-                 <BlockOverview key={0} currPlayer={0==currPlayer} allBlocks={allBlocks.get("red")} color={"red"}/>
-                 <BlockOverview key={1} currPlayer={1==currPlayer} allBlocks={allBlocks.get("yellow")} color={"yellow"}/>
+                 <BlockOverview key={0} currPlayer={0==currPlayer} allBlocks={allBlocks.get(allPlayer.find(player => player.player_id == 0).color)} color={ allPlayer.find(player => player.player_id == 0).color}/>
+                 <BlockOverview key={1} currPlayer={1==currPlayer} allBlocks={allBlocks.get(allPlayer.find(player => player.player_id == 1).color)} color={ allPlayer.find(player => player.player_id == 1).color}/>
             </div>
             <div className="game-field">
+            <button onClick={handleButtonClick}>test Websocket</button>
+            <input value={inputValue} onChange={handleInputChange} />
                 <Gamefield squaresArray={squaresArray}/>
             </div>
             <div className="right-overviews">
-                 <BlockOverview key={2} currPlayer={2==currPlayer} allBlocks={allBlocks.get("green")} color={"green"}/>
-                 <BlockOverview key={3} currPlayer={3==currPlayer} allBlocks={allBlocks.get("blue")} color={"blue"}/>
+                 <BlockOverview key={2} currPlayer={2==currPlayer} allBlocks={allBlocks.get(allPlayer.find(player => player.player_id == 2).color)} color={ allPlayer.find(player => player.player_id == 2).color}/>
+                 <BlockOverview key={3} currPlayer={3==currPlayer} allBlocks={allBlocks.get(allPlayer.find(player => player.player_id == 3).color)} color={ allPlayer.find(player => player.player_id == 3).color}/>
             </div>
         </div>
     );
