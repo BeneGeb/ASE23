6cd5b57eb73e4f399b7cc36ac311b0811ce97e11
Pascal Weider <91870580+PascalWeider@users.noreply.github.com>
2023-12-11 15:20:28 +0100

    chat adaption

--- blokus_django/chat/consumers.py
+++ blokus_django/chat/consumers.py
@@ -1,6 +1,7 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
+from .models import *
 
 
 class ChatConsumer(WebsocketConsumer):
@@ -13,12 +14,24 @@ class ChatConsumer(WebsocketConsumer):
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
+        user_index = text_data_json["user_index"]
         message = text_data_json["message"]
+        username = text_data_json["username"]
         print("receive")
 
+        Chat.objects.create(user_index = user_index, username = username, message = message)
+
+        chat_list = Chat.objects.all()
+
+        json_chat_list = []
+
+        for chat in chat_list:
+            chat_data_json = chat.toJSON()
+            json_chat_list.append(chat_data_json) 
+
         async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat_message", "message": message}
-        )
+                self.room_group_name, {"type": "chat_message", "chatlist": list(json_chat_list)}
+            )
 
     def disconnect(self, close_code):
         async_to_sync(
@@ -29,6 +42,6 @@ class ChatConsumer(WebsocketConsumer):
         )
 
     def chat_message(self, event):
-        message = event["message"]
+        chatlist = event["chatlist"]
         print("send")
-        self.send(text_data=json.dumps({"type": "chat", "message": message}))
+        self.send(text_data=json.dumps({"type": "chat", "chatlist": chatlist}))
--- blokus_django/chat/models.py
+++ blokus_django/chat/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Chat(models.Model):
+    user_index = models.AutoField(primary_key=True)
+    username = models.CharField(max_length= 15)
+    message = models.CharField(max_length = 100)
