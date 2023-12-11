0d1b2ca85d496bab82b2f039321da0c9748640d9
BeneGeb <92448111+BeneGeb@users.noreply.github.com>
2023-12-11 11:03:20 +0100

    Update register.js

--- blokus_django/blokus_react/src/components/register/register.js
+++ blokus_django/blokus_react/src/components/register/register.js
@@ -1,34 +1,40 @@
-import React, { useState } from 'react';
-import '../../styles/register/register.css';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "../../styles/register/register.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
   const [credentials, setCredentials] = useState({
-    name: '',
-    email: '',
-    password: ''
+    name: "",
+    email: "",
+    password: "",
   });
 
   const handleChange = (e) => {
+    const navigate = useNavigate();
     const { name, value } = e.target;
-    setCredentials(prevState => ({
+    setCredentials((prevState) => ({
       ...prevState,
-      [name]: value
+      [name]: value,
     }));
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:8000/api/register', {
+      const response = await axios.post("http://localhost:8000/api/register", {
         name: credentials.name,
         email: credentials.email,
-        password: credentials.password
+        password: credentials.password,
       });
-      console.log('Registration successful:', response.data);
+      console.log("Registration successful:", response.data);
+      navigate("/login");
     } catch (error) {
-      console.error('Registration error:', error.response ? error.response.data : error.message);
+      console.error(
+        "Registration error:",
+        error.response ? error.response.data : error.message
+      );
     }
   };
 
@@ -38,21 +44,51 @@ function Register() {
         <h2>REGISTER</h2>
         <form onSubmit={handleSubmit}>
           <div className="input-group">
-            <input className="register-input" type="text" id="name" name="name" placeholder="NAME" value={credentials.name} onChange={handleChange} required />
+            <input
+              className="register-input"
+              type="text"
+              id="name"
+              name="name"
+              placeholder="NAME"
+              value={credentials.name}
+              onChange={handleChange}
+              required
+            />
           </div>
           <div className="input-group">
-            <input className="register-input" type="email" id="email" name="email" placeholder="EMAIL" value={credentials.email} onChange={handleChange} required />
+            <input
+              className="register-input"
+              type="email"
+              id="email"
+              name="email"
+              placeholder="EMAIL"
+              value={credentials.email}
+              onChange={handleChange}
+              required
+            />
           </div>
           <div className="input-group">
-            <input className="register-input" type="password" id="password" name="password" placeholder="PASSWORD" value={credentials.password} onChange={handleChange} required />
+            <input
+              className="register-input"
+              type="password"
+              id="password"
+              name="password"
+              placeholder="PASSWORD"
+              value={credentials.password}
+              onChange={handleChange}
+              required
+            />
           </div>
           <div className="submit-button-container">
-                <button className="submit-button" type="submit">REGISTER</button>
+            <button className="submit-button" type="submit">
+              REGISTER
+            </button>
           </div>
         </form>
-        <br /><br />
+        <br />
+        <br />
         <Link to="/login">Bereits registriert? Zurück zum Login</Link>
-        </div>
+      </div>
     </div>
   );
 }
