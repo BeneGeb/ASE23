720b333e09b4f05d9db222469c0881239c762a77
Samuel2k1 <91870323+Samuel2k1@users.noreply.github.com>
2023-12-13 00:25:05 +0100

    Ki aufgeben output

--- blokus_django/gameinterface/ki.py
+++ blokus_django/gameinterface/ki.py
@@ -10,15 +10,14 @@ colorMatcher = {
 }
 
 
-
 def ki_perform_move(field, inc_player_index, color_hex):
     print("KI is thinking...")
-    ki_id =  KI.objects.get(player_index = inc_player_index).ki_id
+    ki_id = KI.objects.get(player_index=inc_player_index).ki_id
     color = colorMatcher[color_hex]
     # print(f"player_index_id: {player_index_id}")
     # print(len(field))
     ki = KI.objects.get(ki_id=ki_id)
-    
+
     # print(ki)
 
     # blocks = ki.get_blocks()[3]
@@ -27,11 +26,13 @@ def ki_perform_move(field, inc_player_index, color_hex):
 
     block_variation = Block_variation(
         ki.get_blocks(),  inc_player_index, color, field, ki_id)
-    
+
     print(f"block_variation: {block_variation}")
-    
+
     block_variation = block_variation[random.randint(
         0, len(block_variation)-1)]
+    if block_variation == []:
+        return False
     print(f"block_variation: {block_variation}")
     return block_variation
 
@@ -39,13 +40,16 @@ def ki_perform_move(field, inc_player_index, color_hex):
 def Block_variation(blocks, player_index_id, color, field, ki_id):
     chosen_block = 0
     rev_blocks = list(reversed(blocks))
-    
+
     for index, block in enumerate(rev_blocks):
         liste = ki_move(field, block, player_index_id, color)
         if liste != []:
             chosen_block = index
             print(f"Acher{index}")
             break
+    if liste == []:
+        print("KI has not found a move")
+        return []
     # wenn es leer game over
     liste = [list(item) for item in set(tuple(row) for row in liste)]
     # blocks = reversed(blocks)
