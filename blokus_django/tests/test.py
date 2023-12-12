7912cd5679383e553f6e9b7245837e79232da570
Yeaulo <yichenzhang929@gmail.com>
2023-12-12 22:33:48 +0100

    Update test.py

--- blokus_django/tests/test.py
+++ blokus_django/tests/test.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 import time
+import json
 
 class AuthTestCase(TestCase):
 
@@ -72,3 +73,28 @@ class AuthTestCase(TestCase):
         }
         response = self.client.post('/api/login/', login_data, format='json')
         self.assertNotEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+class LobbyTestCase(TestCase):
+    
+    def setUp(self):
+        self.client = APIClient()
+
+    def register_and_login_user(self, email, password):
+        user_data = {
+            'name': 'TestUser',
+            'email': email,
+            'password': password
+        }
+        self.client.post('/api/register/', user_data, format='json')
+        login_data = {
+            'email': email,
+            'password': password
+        }
+        response = self.client.post('/api/login/', login_data, format='json')
+        return json.loads(response.content)
+
+    def test_join_lobby_success(self):
+        user_credentials = self.register_and_login_user('testuser@example.com', 'password123')
+        access_token = user_credentials.get('access_token')
+        response = self.client.post('/api/join_lobby/', {'access_token': access_token}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
