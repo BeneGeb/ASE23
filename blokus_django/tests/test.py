7880c8dbd35f46ca32633705859110c75d28c79c
Yeaulo <yichenzhang929@gmail.com>
2023-12-12 21:45:25 +0100

    fix final test #3

--- blokus_django/tests/test.py
+++ blokus_django/tests/test.py
@@ -26,49 +26,49 @@ class AuthTestCase(TestCase):
         response = self.client.post('/api/register/', incomplete_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    # def test_register_with_existing_email(self):
-    #     self.client.post('/api/register/', {'name': 'User1', 'email': 'user@example.com', 'password': 'password123'}, format='json')
-    #     duplicate_email_data = {
-    #         'name': 'User2',
-    #         'email': 'user@example.com',
-    #         'password': 'newpassword123'
-    #     }
-    #     response = self.client.post('/api/register/', duplicate_email_data, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_register_with_existing_email(self):
+        self.client.post('/api/register/', {'name': 'User1', 'email': 'user1@example.com', 'password': 'password123'}, format='json')
+        duplicate_email_data = {
+            'name': 'User2',
+            'email': 'user1@example.com',
+            'password': 'newpassword123'
+        }
+        response = self.client.post('/api/register/', duplicate_email_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    # def test_register_with_invalid_email(self):
-    #     invalid_email_data = {
-    #         'name': 'TestUser',
-    #         'email': 'notanemail',
-    #         'password': 'password123'
-    #     }
-    #     response = self.client.post('/api/register', invalid_email_data, format='json')
-    #     self.assertNotEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_register_with_invalid_email(self):
+        invalid_email_data = {
+            'name': 'TestUser',
+            'email': 'notanemail',
+            'password': 'password123'
+        }
+        response = self.client.post('/api/register', invalid_email_data, format='json')
+        self.assertNotEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    # def test_login_success(self):
-    #     user_data = {
-    #         'name': 'TestUser',
-    #         'email': 'test@example.com',
-    #         'password': 'password123'
-    #     }
-    #     response = self.client.post('/api/register/', user_data, format='json')
-    #     login_data = {
-    #         'email': 'test@example.com',
-    #         'password': 'password123'
-    #     }
-    #     response = self.client.post('/api/login/', login_data, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_login_success(self):
+        user_data = {
+            'name': 'TestUser1',
+            'email': 'testuser@example.com',
+            'password': 'password123'
+        }
+        response = self.client.post('/api/register/', user_data, format='json')
+        login_data = {
+            'email': 'testuser@example.com',
+            'password': 'password123'
+        }
+        response = self.client.post('/api/login/', login_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    # def test_login_with_invalid_data(self):
-    #     user_data = {
-    #         'name': 'TestUser',
-    #         'email': 'test@example.com',
-    #         'password': 'password123'
-    #     }
-    #     response = self.client.post('/api/register/', user_data, format='json')
-    #     login_data = {
-    #         'email': 'test@example.com',
-    #         'password': 'wrongpassword'
-    #     }
-    #     response = self.client.post('/api/login/', login_data, format='json')
-    #     self.assertNotEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_login_with_invalid_data(self):
+        user_data = {
+            'name': 'TestUser5',
+            'email': 'testuser5@example.com',
+            'password': 'password123'
+        }
+        response = self.client.post('/api/register/', user_data, format='json')
+        login_data = {
+            'email': 'testuser5@example.com',
+            'password': 'wrongpassword'
+        }
+        response = self.client.post('/api/login/', login_data, format='json')
+        self.assertNotEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
